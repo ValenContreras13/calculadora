1b38675e6283a874deae0d83821bc8c0b728574d
Valentina Contreras <valentinacr1309@gmail.com>
2025-05-22 10:15:40 -0500

    Calculadora

---
+++ script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+
+let num;
+let num2;
+let resultado;
+
+function suma(num,num2){
+return num+num2;
+}
+
+function mostrarSuma(){
+num = parseInt(document.getElementById("num").value);
+num2 = parseInt(document.getElementById("num2").value);
+resultado=suma(num,num2);
+console.log(resultado);
+document.getElementById("resultado").textContent = "El resultado es: " + resultado;
+}
+
+
+function resta(num1,num2){
+return num1-num2;
+}
+
+function mostrarResta(){
+num = parseInt(document.getElementById("num").value);
+num2 = parseInt(document.getElementById("num2").value);
+resultado=resta(num,num2);
+console.log(resultado);
+document.getElementById("resultado").textContent = "El resultado es: " + resultado;
+}
+
+function multiplicacion(num1,num2){
+return num1*num2;
+}
+
+function mostrarMultiplicacion(){
+num = parseInt(document.getElementById("num").value);
+num2 = parseInt(document.getElementById("num2").value);
+resultado=multiplicacion(num,num2);
+console.log(resultado);
+document.getElementById("resultado").textContent = "El resultado es: " + resultado;
+}
+
+function division(num1,num2){
+return num1 / num2;
+}
+
+function mostrarDivision(){
+num = parseInt(document.getElementById("num").value);
+num2 = parseInt(document.getElementById("num2").value);
+resultado=division(num,num2);
+console.log(resultado);
+document.getElementById("resultado").textContent = "El resultado es: " + resultado;
+}
